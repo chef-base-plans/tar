44d99a7798a00fb3421d4ac8e63b8f54645a7a4c
Davy McAleer <dmcaleer@chef.io>
2020-05-26 15:21:18 +0100

    updated tests and attributes changed to inputs
    Signed-off-by: Davy McAleer <dmcaleer@chef.io>

--- controls/tar_works.rb
+++ controls/tar_works.rb
@@ -1,11 +1,10 @@
-tar_test = attribute('tar_test', default: '/bin/tar -cvf run.tar /hab/svc/tar/hooks/run')
+tar_test = input('tar_test', value: '/bin/tar -cvf run.tar /hab/svc/tar/hooks/run')
 
 describe bash(tar_test) do
   its('stdout') { should match /run/ }
-  its('stderr') { should eq '' }
   its('exit_status') { should eq 0 }
 end
 
-describe file('/hab/svc/tar/run.tar') do
+describe file('run.tar') do
   it { should exist }
 end
