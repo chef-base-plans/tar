7f3132dd90101b5e2b2a3e99e72383ee0b3d213c
Ubuntu <ubuntu@ip-172-31-57-32.us-west-2.compute.internal>
2022-02-04 13:46:09 +0000

    upgraded tar to v1.34

--- x86_64-linux-kernel2/plan.sh
+++ x86_64-linux-kernel2/plan.sh
@@ -2,7 +2,7 @@ pkg_name=tar
 pkg_origin=core
 # tar xf for 1.32 and greater is failing on kernel2 with `Cannot change mode to rwxrwxr-x: Invalid argument`
 # sticking with 1.30 on linux2 for for now
-pkg_version=1.30
+pkg_version=1.34
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 GNU Tar provides the ability to create tar archives, as well as various other \
@@ -11,7 +11,7 @@ kinds of manipulation.\
 pkg_upstream_url="https://www.gnu.org/software/tar/"
 pkg_license=('GPL-3.0-or-later')
 pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="4725cc2c2f5a274b12b39d1f78b3545ec9ebb06a6e48e8845e1995ac8513b088"
+pkg_shasum=03d908cf5768cfe6b7ad588c921c6ed21acabfb2b79b788d1330453507647aed
 pkg_deps=(
   core/glibc
   core/acl
